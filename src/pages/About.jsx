aa5bc92bc012e762b74f62aa09216fdbeb46926c
Sara <Lokifeathers@gmail.com>
2025-11-13 18:59:40 -0500

    Update About section with graduation details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,7 +19,7 @@ function About() {
                                 Hi there! I'm Sara Ryan, a passionate and aspiring developer based in Virginia. My journey into coding and development is fueled by my lifelong love for gaming and technology. What truly drew me to development was seeing how a few lines of code could transform into interactive experiences that users can engage with worldwide. Crazy to think just what can be created at the tips of your fingers!
                             </p>
                             <p>
-                                Currently, I'm enrolled in the University of Richmond Coding Bootcamp with EdX, focusing on Full Stack Development. I'm learning everything from HTML and CSS to Node.js and React, and I'm excited to apply these skills to real-world projects.
+                                Recently, I was enrolled in the University of Richmond Coding Bootcamp with EdX, focusing on Full Stack Development. I graduated from the program, learning everything from HTML and CSS to Node.js and React, and I'm excited to apply these skills to real-world projects.
                             </p>
                             <p>
                                 One of my proudest achievements so far is creating a custom script for RedM Roleplay game servers. This script enhances the community experience by bringing real-life competitions to the game world and furthering character stories, perfectly blending my passion for gaming with my newfound coding skills.
@@ -28,7 +28,7 @@ function About() {
                                 Looking ahead, I'm eager to explore emerging web technologies, contribute to open-source projects, and develop innovative web applications that solve real-world problems.
                             </p>
                             <p>
-                                When I'm not coding, you can find me gaming, attending tech meetups, or reading a good book. I'm thrilled to be on this exciting journey and can't wait to see where it takes me in the world of web development! Feel free to connect with me on LinkedIn or explore my projects on GitHub - I'm always excited to collaborate with fellow developers and tech enthusiasts.
+                                When I'm not coding, you can find me gaming, researching technologies, coding, or reading a good book. I'm thrilled to be on this exciting journey and can't wait to see where it takes me in the world of web development! Feel free to connect with me on LinkedIn or explore my projects on GitHub - I'm always excited to collaborate with fellow developers and tech enthusiasts.
                             </p>
                         </div>
                     </div>
